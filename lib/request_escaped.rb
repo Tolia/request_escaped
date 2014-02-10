a3fbc092f8537be80b99b47fe840817c3f049e14
tolia <toliademidov@gmail.com>
2014-02-10 18:16:08 +0400

    update

--- lib/request_escaped.rb
+++ lib/request_escaped.rb
@@ -3,12 +3,16 @@ module RequestEscaped
   class Rack::Request
 
     def escaped?
-      @env.key? :_escaped_fragment_
+      @env._params.key? :_escaped_fragment_
     end
 
     def escaped_fragment
-      @env[:_escaped_fragment_]
+      @env["_params._escaped_fragment_"]
     end
+
+    def escaped_fragment?(fragment)
+      @env._params
+    end    
 
   end
 
